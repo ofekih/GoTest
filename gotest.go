feaa819921b8451d89f0bbd0fbd863267227b6a5
Ofek Gila <5705052+ofekih@users.noreply.github.com>
2022-06-06 11:48:41 -0700

    mymodule: changes for v0.1.0

---
+++ gotest.go
@@ -0,0 +1,17 @@
+package GoTest
+
+import (
+	"errors"
+	"fmt"
+)
+
+func Hello(name string) (string, error) {
+	// If no name was given, return an error message.
+	if name == "" {
+		return "", errors.New("empty name")
+	}
+
+	// Return a greeting that embeds the name in a message.
+	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+	return message, nil
+}
